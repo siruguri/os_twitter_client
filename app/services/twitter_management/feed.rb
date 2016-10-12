e94285dba38aea881c62877d1b751afb8a5919a1
Sameer Siruguri <siruguri@gmail.com>
2016-10-12 09:23:24 -0700

    Close #7
    * Correct mobile behavior of image overlay

--- app/services/twitter_management/feed.rb
+++ app/services/twitter_management/feed.rb
@@ -5,14 +5,11 @@ module TwitterManagement
       refresh_list = []
       now = Time.now
 
-      top = Tweet.top_of_feed(profile)
-      if top.nil? or top < (now - 24.hours)
-        profile.friends.where('last_tweet_time is not null and last_tweet_time > ?', DateTime.now - 100.days).
-          order(last_tweet_time: :desc).each do |leader_profile|
-          TwitterFetcherJob.perform_later leader_profile, 'tweets', token: token, direction: 'newer',
-                                          refresh_bookmark: "#{profile.user&.email}.twitter.bookmark"
-          refresh_list << "#{leader_profile.handle} (#{(now - leader_profile.last_tweet_time)/(60*60*24)})"
-        end
+      profile.friends.where('last_tweet_time is not null and last_tweet_time > ?', DateTime.now - 100.days).
+        order(last_tweet_time: :desc).each do |leader_profile|
+        TwitterFetcherJob.perform_later leader_profile, 'tweets', token: token, direction: 'newer',
+                                        refresh_bookmark: "#{profile.user&.email}.twitter.bookmark"
+        refresh_list << "#{leader_profile.handle} (#{(now - leader_profile.last_tweet_time)/(60*60*24)})"
       end
 
       refresh_list
